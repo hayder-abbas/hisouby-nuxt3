e312acc630f1aa1a64b046687bfedd1a30cfa481
hayder-abbas <haydarabbas40@proton.me>
2026-03-06 16:50:31 +0300

    fit: Update favicon and add image for logo

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,7 +1,15 @@
 <template>
-	<div
-		class="font-bold text-2xl text-slate-900 hover:text-blue-600 transition-colors duration-200"
+	<NuxtLink
+		to="/"
+		class="flex items-center gap-2 font-bold text-2xl text-slate-900 hover:text-blue-600 transition-colors duration-200"
 	>
-		<NuxtLink to="/"> حيسوبي </NuxtLink>
-	</div>
+		<img
+			src="/images/hisuby-logo.png"
+			alt="Logo"
+			class="w-12 h-12 rounded-lg object-cover"
+			loading="lazy"
+		/>
+		<span class="sr-only">Go to homepage</span>
+		<span>حيسوبي</span>
+	</NuxtLink>
 </template>
